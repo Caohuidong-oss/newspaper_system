cfd8584c483d8d90230b05ccbf8f9cf8975d28f6
小小东 <Caohuidong-oss@users.noreply.github.com>
2026-07-26 16:30:16 +0800

    fix: miniprogram 所有文件 GBK→UTF-8 转换

--- miniprogram/pages/newspapers/newspapers.js
+++ miniprogram/pages/newspapers/newspapers.js
@@ -27,7 +27,7 @@ Page({
         this.setData({ newspapers: list });
       })
       .catch(err => {
-        wx.showToast({ title: err.message || '加载失败', icon: 'none' });
+        wx.showToast({ title: err.message || '鍔犺浇澶辫触', icon: 'none' });
       })
       .finally(() => {
         this.setData({ loading: false });
